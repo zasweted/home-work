a1d2fc14b22a6eedbe575cf2ad315ed86c0e76d4
Aleksandr Vasiljev <zaswet@gmail.com>
2022-07-23 17:16:22 +0300

    homework

--- src/Components/trees.jsx
+++ src/Components/trees.jsx
@@ -1,11 +1,37 @@
 import { useState } from "react";
 
 function Trees(){
-    const oakStyle = {fontSize: '20px'}
+    /*azuolas start */
+    const oakStyle = {fontSize: '20px'};
     const [oakTree, setOakTree] = useState([]);
     const addOakTree = () => {
         setOakTree(x => [...x, ''])
+    };
+    /*azuolas end */
+
+    /*berzas start */
+    const birchStyle = {fontSize: '20px', color: '#000'};
+    const [birchTree, setBirchTree] = useState([]);
+    const addBirchTree = () => {
+        setBirchTree(x => [...x, ''])
     }
+    /*berzas end */
+
+    /*uosis start */
+    const ashTreeStyle = {fontSize: '20px'};
+    const [ashTree, setAshTree] = useState([]);
+    const addAshTree = () => {
+        setAshTree(x => [...x, ''])
+    };
+
+    /*uosis end */
+
+    const reset = () => {
+        setOakTree(_ => [])
+        setBirchTree(_ => [])
+        setAshTree(_ => [])
+    };
+
 
     return(
 
@@ -21,13 +47,30 @@ function Trees(){
                 }
 
             </div>
-            <div className="grass"></div>
-            <div className="grass"></div>
+            <div className="grass">
+
+                    {
+                        birchTree.map((_, i) => <div className="birch-tree" key={i}><p style={birchStyle}>Berzas</p></div>)
+                    }
+
+            </div>
+            <div className="grass">
+
+                    {
+                        ashTree.map((_, i) => <div className="ash-tree" key={i}><p style={ashTreeStyle}>Uosis</p></div>)
+                    }
+
+            </div>
         </div>
         <div className="button-container">
             <button onClick={addOakTree}>azuolas</button>
-            <button>berzas</button>
-            <button>uosis</button>
+            <button onClick={addBirchTree}>berzas</button>
+            <button onClick={addAshTree}>uosis</button>
+        </div>
+        <div style={{
+            marginBottom: '80px'
+        }}>
+            <button onClick={reset}>reset</button>
         </div>
         </>
 
